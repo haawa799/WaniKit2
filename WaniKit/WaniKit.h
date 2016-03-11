580578e26d9b2052b0afc8b066d373b31a7ca26c
Andrew Kharchyshyn <haawaplus@gmail.com>
2016-03-11 10:58:30 +0200

    Initial Commit

---
+++ WaniKit/WaniKit.h
@@ -0,0 +1,19 @@
+//
+//  WaniKit.h
+//  WaniKit
+//
+//  Created by Andriy K. on 3/11/16.
+//  Copyright © 2016 Andriy K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WaniKit.
+FOUNDATION_EXPORT double WaniKitVersionNumber;
+
+//! Project version string for WaniKit.
+FOUNDATION_EXPORT const unsigned char WaniKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WaniKit/PublicHeader.h>
+
+
